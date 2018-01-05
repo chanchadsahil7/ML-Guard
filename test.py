8b5d05c1d46b54cd5959d5014169f16577f63aa9
RaviTejaKomma <ravieee929374s@gmail.com>
2018-01-05 12:50:44 +0530

    Added test file, results.txt and test images folder

---
+++ test.py
@@ -0,0 +1,45 @@
+
+import os
+from Main import main_func,initialize_train_model
+
+
+## replace this path with your folders path where the 'car_front_images' folder and 'result.txt' are present ##
+BASE_DIR = 'F:\DATASCIENCE\Solivar_Work\MLGuard\\'
+
+def test():
+    initialize_train_model()
+
+    files = os.listdir(BASE_DIR + 'car_front_images')
+    files.sort(key=lambda name: int(name.split('.')[0].split('car')[1]))
+
+    fp = open(BASE_DIR + 'results.txt')
+    contents = fp.read().split('\n')
+
+    i = 0
+    count1 = count2 = count3 = total_count2 = 0
+
+    for name in files:
+        result = main_func(BASE_DIR + 'car_front_images\\' + name)
+        if result == 0:
+            print "No chars were found in the plate"
+        else:
+            # print result, contents[i]
+            if result == contents[i]:
+                count1 += 1
+            if len(result) == len(contents[i]):
+                total_count2 += len(contents[i])
+                j = 0
+                while j < len(contents[i]) and j < len(result):
+                    if contents[i][j] == result[j]:
+                        count2 += 1
+                    j += 1
+            else:
+                count3 += 1
+        i += 1
+    print "Number of plates correctly predicted : ", count1
+    print count2, " predicted correctly out of ", total_count2
+    print "Number of plates whose length is more/less than the original ", count3
+    return
+
+if __name__ == '__main__':
+    test()
